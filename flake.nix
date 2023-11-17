3d4f6a40bd1edf9fdb4959051edc172473d9544d
Maix0 <maix522@gmail.com>
2023-11-17 23:18:09 +0100

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -14,10 +14,28 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        src_patched = pkgs.stdenv.mkDerivation {
+          name = "c_formatter_42-patched";
+          src = c_formatter_42_src;
+          #patch = ./rename-binary.patch;
+          installPhase = ''
+            mkdir -p $out/c_formatter_42/{data,formatters}
+            cp $src/setup.{py,cfg} $src/LICENSE $src/README.md $out
+            cp $src/c_formatter_42/*.py $out/c_formatter_42
+            cp $src/c_formatter_42/data/*.py $out/c_formatter_42/data
+            cp $src/c_formatter_42/data/.clang-format $out/c_formatter_42/data
+            cp $src/c_formatter_42/formatters/*.py $out/c_formatter_42/formatters
+            ln -s ${pkgs.writeShellScript "clang-format-linux" 
+            '' ${pkgs.clang-tools}/bin/clang-format "$@" ''
+            } $out/c_formatter_42/data/clang-format-linux
+            ${pkgs.tree}/bin/tree $out
+          '';
+        };
         c_formatter_42_drv = pkgs.python311Packages.buildPythonApplication {
           pname = "c_formatter_42";
           version = "1.0";
-          src = c_formatter_42_src;
+          src = src_patched;
+          buildInputs = [pkgs.clang-tools];
         };
       in {
         packages = rec {
